4f971a36080042c5ec72797f37bb0e6e0ee0a0c5
Leon Reindl <leonreindl@lolo.fritz.box>
2017-10-12 20:51:01 +0200

    fixes

--- app/Index.jsx
+++ app/Index.jsx
@@ -1,23 +1,10 @@
 import React            from 'react';
 import { render }       from 'react-dom';
-import {
-    Router,
-    Route,
-    browserHistory,
-}        from 'react-router';
 import 'whatwg-fetch';
 import App              from 'components/App.jsx';
 
 
 
 render(
-    <Router
-        history={ browserHistory }
-    >
-        <Route
-            path="/"
-            component={ App }
-            history={ browserHistory }
-        />
-    </Router>
+    <App />
     , document.getElementById( 'app' ) );
